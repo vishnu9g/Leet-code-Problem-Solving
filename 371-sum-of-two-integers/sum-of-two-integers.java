dab93ed40bf702ae697efca710439205e33d65db
Vishnu S <vishnusaravanansv@gmail.com>
2025-03-21 11:52:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (90.19%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+class Solution {
+    public int getSum(int a, int b) {
+        // BigInteger a1 = new BigInteger(a);
+        // BigInteger a2 = new BigInteger(b);
+        // BigInteger ans = a1.add(a2);
+        int ans = a+b;
+        return ans;
+    }
+}
