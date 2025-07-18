3094f1609c79f6c0514032ca15379b9b3a13f92e
Vishnu S <vishnusaravanansv@gmail.com>
2025-07-18 16:18:40 +0530

    Time: 103 ms (39.88%) | Memory: 49.6 MB (26.71%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -2,8 +2,9 @@ class Solution {
     public int countPrimes(int n) {
         if (n < 2) return 0;
         boolean[] isPrime = new boolean[n];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = isPrime[1] = false;
+        for(int i=2;i<n;i++){
+            isPrime[i]=true;
+        }
         for (int i = 2; i * i < n; i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j < n; j += i) {
@@ -11,10 +12,10 @@ class Solution {
                 }
             }
         }
-        int count = 0;
+        int c = 0;
         for (boolean prime : isPrime) {
-            if (prime) count++;
+            if (prime) c++;
         }
-        return count;
+        return c;
     }
 }
