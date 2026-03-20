b566176cc35f95790b6f35f55bad11c33566cc1b
Vishnu S <vishnusaravanansv@gmail.com>
2026-03-20 09:48:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int[] numberGame(int[] nums) {
+    public int[] numberGame(int[] nums){
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i=i+2){
-               int temp = nums[i];
-               nums[i]=nums[i+1];
-               nums[i+1]=temp;
+        for (int i=0;i<nums.length;i=i+2){
+            int temp=nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=temp;
         }
         return nums;
     }
