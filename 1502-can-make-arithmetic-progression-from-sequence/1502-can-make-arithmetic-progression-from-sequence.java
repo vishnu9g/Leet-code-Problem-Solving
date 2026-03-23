ff07740675d73b6c3aea7cb3f3d1b285f12f5c93
Vishnu S <vishnusaravanansv@gmail.com>
2026-03-23 10:44:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,12 +1,10 @@
 class Solution {
-    public boolean canMakeArithmeticProgression(int[] arr) {
+    public boolean canMakeArithmeticProgression(int[] arr){
+        int d = Math.abs(arr[1]-arr[0]);
         Arrays.sort(arr);
-        if(arr.length<3) return true;
-        int d = arr[1]-arr[0];
         for(int i=1;i<arr.length-1;i++){
-            if(arr[i+1]-arr[i]!=d){
-                return false;
-            }
+            int d1=Math.abs(arr[i+1]-arr[i]);
+            if(d1!=d) return false;
         }
         return true;
     }
